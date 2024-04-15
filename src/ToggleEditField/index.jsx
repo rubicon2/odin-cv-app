976c9806831e8fa7e3e9f54ad3f3fd483f08a494
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-04-15 19:13:34 +0100

    ToggleEditField can now take an inputType prop

--- src/ToggleEditField/index.jsx
+++ src/ToggleEditField/index.jsx
@@ -1,11 +1,11 @@
 import './index.css';
 
-export default function ToggleEditField({fieldName, onChange, value, edit}) {
+export default function ToggleEditField({fieldName, inputType = 'text', value, onChange, edit}) {
   return (
     <div className='toggle-edit-field'>
       <label className='toggle-edit-field-label' htmlFor={fieldName}>{fieldName}</label>
       {edit && 
-        <input className='toggle-edit-field-input' id={fieldName} onChange={onChange} value={value}></input>
+        <input type={inputType} className='toggle-edit-field-input' id={fieldName} onChange={onChange} value={value}></input>
       }
       {!edit &&
         <span className='toggle-edit-field-readonly'>{value || 'None stated'}</span>
