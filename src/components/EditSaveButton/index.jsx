e2c4089f4d8709efa2bdf793a1bc04cb1eb91479
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-05 12:11:59 +0100

    Convert EditSaveButton to styled component

--- src/components/EditSaveButton/index.jsx
+++ src/components/EditSaveButton/index.jsx
@@ -1,13 +1,26 @@
-import './index.css';
+import styled from 'styled-components';
 
 export default function EditSaveButton({
   type = 'submit',
   onClick,
   submitted = false,
 }) {
+  const Button = styled.button`
+    align-self: flex-end;
+    min-width: 7em;
+    margin-top: 1rem;
+    background-color: var(--light-accent-color);
+    color: var(--light-color);
+
+    &:hover,
+    &:focus {
+      background-color: var(--dark-accent-color);
+    }
+  `;
+
   return (
-    <button className="edit-save-button" type={type} onClick={onClick}>
+    <Button type={type} onClick={onClick}>
       {submitted ? 'Edit' : 'Save'}
-    </button>
+    </Button>
   );
 }
