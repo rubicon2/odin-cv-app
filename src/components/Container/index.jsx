862d4513609e3da5ba55f98f3406ea6308fbeb6a
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-05 12:06:46 +0100

    Convert Container to styled component

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,5 +1,22 @@
-import './index.css';
+import styled from 'styled-components';
 
 export default function Container({ children, className = '' }) {
-  return <div className={`container ${className}`}>{children}</div>;
+  const Container = styled.div`
+    padding: 2rem;
+
+    background-color: var(--light-color);
+    color: var(--dark-color);
+    border-radius: 15px;
+
+    display: flex;
+    flex-direction: column;
+
+    box-shadow: 5px 5px 5px 5px rgb(0, 0, 0, 0.2);
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  `;
+
+  return <Container className={className}>{children}</Container>;
 }
